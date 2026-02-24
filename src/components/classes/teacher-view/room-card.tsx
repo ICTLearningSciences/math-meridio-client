394ef6f79511dfcd21b7e5c44b340bc62dac8108
kycarr <whizbon@gmail.com>
2026-02-24 10:24:33 -0800

    Instructor can toggle player as active/inactive

--- src/components/classes/teacher-view/room-card.tsx
+++ src/components/classes/teacher-view/room-card.tsx
@@ -6,13 +6,24 @@ The full terms of this copyright and license should always be found in the root 
 */
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import * as motion from 'motion/react-client';
 import { ArrowForward } from '@mui/icons-material';
-import { Card, CardContent, IconButton, Typography } from '@mui/material';
+import {
+  Card,
+  CardContent,
+  IconButton,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+
 import { Room } from '../../../store/slices/game/types';
 import { GAMES } from '../../../game/types';
 import AvatarSprite from '../../avatar-sprite';
 import ProgressBar from '../../progress-bar';
 import { Classroom } from '../../../store/slices/educational-data/types';
+import { useWithEducationalData } from '../../../store/slices/educational-data/use-with-educational-data';
+import { TwoOptionDialog } from '../../dialog';
+import { Player } from '../../../store/slices/player/types';
 
 export default function RoomCard(props: {
   classroom: Classroom;
@@ -21,10 +32,27 @@ export default function RoomCard(props: {
 }): JSX.Element {
   const { room, classroom, classes } = props;
   const navigate = useNavigate();
+  const { togglePlayerPausedInRoomStatus } = useWithEducationalData();
+  const [pausePlayer, setPausePlayer] = React.useState<Player>();
+  const [updating, setUpdating] = React.useState<boolean>(false);
+
   const game = GAMES.find((g) => g.id === room?.gameData.gameId);
 
   const enterRoom = () => {
     navigate(`/classes/${classroom._id}/room/${room._id}`);
+  };
+
+  const onTogglePause = async () => {
+    if (!pausePlayer) return;
+    setUpdating(true);
+    try {
+      await togglePlayerPausedInRoomStatus(room._id, pausePlayer._id);
+      setUpdating(false);
+      setPausePlayer(undefined);
+    } catch {
+      setUpdating(false);
+      setPausePlayer(undefined);
+    }
   };
 
   return (
@@ -71,13 +99,41 @@ export default function RoomCard(props: {
                   : minsSince < 60 * 24
                   ? `${Math.floor(minsSince / 60)} HOURS AGO`
                   : `${Math.floor(minsSince / (60 * 24))} DAYS AGO`;
+              if (isPaused) {
+                return (
+                  <Tooltip key={player._id} title="Unpause player">
+                    <div className="column center-div">
+                      <motion.div
+                        className="column center-div"
+                        whileHover={{ scale: 1.05, filter: 'brightness(0.8)' }}
+                        onClick={() => setPausePlayer(player)}
+                      >
+                        <AvatarSprite
+                          player={player}
+                          bgColor="rgb(218, 183, 250)"
+                          isPaused={isPaused}
+                        />
+                      </motion.div>
+
+                      <Typography
+                        variant="body2"
+                        fontSize={12}
+                        fontWeight="bold"
+                        align="center"
+                        style={{ marginTop: 5 }}
+                      >
+                        {player.name}
+                      </Typography>
+                      <Typography fontSize={10} fontWeight="lighter">
+                        PAUSED
+                      </Typography>
+                    </div>
+                  </Tooltip>
+                );
+              }
               return (
                 <div key={player._id} className="column center-div">
-                  <AvatarSprite
-                    player={player}
-                    bgColor="rgb(218, 183, 250)"
-                    isPaused={isPaused}
-                  />
+                  <AvatarSprite player={player} bgColor="rgb(218, 183, 250)" />
                   <Typography
                     variant="body2"
                     fontSize={12}
@@ -88,15 +144,15 @@ export default function RoomCard(props: {
                     {player.name}
                   </Typography>
                   <Typography fontSize={10} fontWeight="lighter">
-                    {isPaused
-                      ? 'PAUSED'
-                      : isActive
+                    {isActive
                       ? room.gameData.playersStatusRecord[player._id]
                           .computedState
                       : activityStr}
                   </Typography>
-                  {!isPaused && (
-                    <div
+                  <Tooltip title="Pause player">
+                    <motion.div
+                      whileHover={{ scale: 1.05, filter: 'brightness(0.8)' }}
+                      onClick={() => setPausePlayer(player)}
                       style={{
                         position: 'absolute',
                         marginRight: -30,
@@ -108,8 +164,8 @@ export default function RoomCard(props: {
                           : 'white',
                         border: `1px solid ${isActive ? 'white' : 'black'}`,
                       }}
-                    />
-                  )}
+                    ></motion.div>
+                  </Tooltip>
                 </div>
               );
             })}
@@ -127,6 +183,29 @@ export default function RoomCard(props: {
           </IconButton>
         </CardContent>
       </Card>
+      {pausePlayer && (
+        <TwoOptionDialog
+          title={`Mark ${pausePlayer.name} as ${
+            room.gameData.playersStatusRecord[pausePlayer._id].pausedByAdmin
+              ? 'active'
+              : 'inactive'
+          }?`}
+          open={Boolean(pausePlayer)}
+          actionInProgress={updating}
+          option1={{
+            display: `${
+              room.gameData.playersStatusRecord[pausePlayer._id].pausedByAdmin
+                ? 'Unpause'
+                : 'Pause'
+            } Player`,
+            onClick: () => onTogglePause(),
+          }}
+          option2={{
+            display: 'Cancel',
+            onClick: () => setPausePlayer(undefined),
+          }}
+        />
+      )}
     </div>
   );
 }
